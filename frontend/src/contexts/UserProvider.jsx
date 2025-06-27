59f6215217debee9a405776c579e6e28c117fe17
EmptyXNothing <Mr.Pisulkin666777@gmail.com>
2025-06-27 20:48:18 +0400

    Add context

---
+++ frontend/src/contexts/UserProvider.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+const UserProvider = (props) => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
+
+  const logIn = (data) => {
+    localStorage.setItem("user", JSON.stringify(data))
+    setUser(() => data)
+  }
+
+  const logOut = () => {
+    localStorage.removeItem("user")
+    setUser(() => null)
+  }
+
+  const value = {
+    user,
+    logIn,
+    logOut,
+  };
+
+  return (
+    <UserContext.Provider value={value}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
