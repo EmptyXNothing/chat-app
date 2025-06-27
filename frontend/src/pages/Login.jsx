57f2ace34603ed343a4f6a559c5eeb3a8dd05a7d
EmptyXNothing <Mr.Pisulkin666777@gmail.com>
2025-06-27 20:54:20 +0400

    Login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,68 @@
+import '../styles/Login.css';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { UserContext } from '../Contexts/UserProvider.jsx';
+import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react';
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(5, 'Too Short!')
+    .max(35, 'Too Long!')
+    .required('Required'),
+  password: Yup.string()
+    .min(5, 'Too Short!')
+    .max(35, 'Too Long!')
+    .required('Required'),
+});
+
+const Login = () => {
+  const navigate = useNavigate()
+  const { logIn } = useContext(UserContext);
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: SignupSchema,
+    onSubmit: async (values) => { 
+      const { data } = await axios.post('/api/v1/login', values)
+      logIn(data)
+      navigate('/')
+    },
+  });
+  return (
+    <div className="form">
+      <form onSubmit={formik.handleSubmit}>
+        <div className="username">
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.username}
+          />
+          <p className="error">{formik.errors.username}</p>
+        </div>
+        <div className="password">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          <p className="error">{formik.errors.password}</p>
+        </div>
+        <div className='btn'>
+          <button type="submit">Log in</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
